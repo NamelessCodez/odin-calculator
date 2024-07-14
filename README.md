58d7cc73b74134c2ff99f95c8f62de79120e9af5
Satyam <satyamsharma2106@gmail.com>
2024-07-14 17:38:13 +0530

    make decent progess in using just operation and number keys

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # odin-calculator
+algorithm :
+listen for num key clicks, ignore other key clicks before a num key is clicked
+after first num key click, continue listening for num keys, and allow operator keys to be clicked
+if after that operator key is clicked, start to record second number by listening for num keys,
+ if after entering both number, operator key is pressed, show the result and store it in first number variable
--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+let firstNumStr = '';
+let secondNumStr = '';
+let operator = '';
+
+const display = document.querySelector('.display');
+const numberKeys = document.querySelectorAll('.number');
+const mainOperations = document.querySelectorAll('.main-operation');
+const equalsKey = document.querySelector('.equals');
+
+numberKeys.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let numClicked = btn.textContent;
+        // if operator exists first number has already been assigned, so now take the second number
+        if (operator) {
+            secondNumStr += numClicked;
+            display.textContent = secondNumStr;
+        }
+        // if oper
+        else {
+            firstNumStr += numClicked;
+            display.textContent = firstNumStr;
+        }
+        console.log(firstNumStr);
+        console.log(secondNumStr);
+    });
+});
+
+mainOperations.forEach((btn) => {
+    btn.addEventListener('click',() => {
+        let selectedOperator = btn.textContent
+        // ignore clicks if the first number has not been assigned
+        if (firstNumStr) {
+            operator = selectedOperator;
+            display.textContent = 0;
+        } 
+        if (firstNumStr && secondNumStr) {
+            firstNumStr = operate(operator, parseInt(firstNumStr), parseInt(secondNumStr));
+            secondNumStr = '';
+            display.textContent = firstNumStr;
+            operator = selectedOperator;
+        }
+    });
+});
+
+equalsKey.addEventListener('click', () => {
+    console.log(firstNumStr)
+    if (firstNumStr && secondNumStr) {
+        firstNumStr = operate(operator, parseInt(firstNumStr), parseInt(secondNumStr)).toString();
+        secondNumStr = '';
+        display.textContent = firstNumStr;
+    }
+});
+
 function add(a, b) {
     return a + b;
 }
@@ -9,4 +62,17 @@ function multiply(a, b) {
 }
 function divide(a, b) {
     return a / b;
+}
+
+function operate (operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
 }

fda196289d62a88ae6aef9a666f338897bf4deca
Satyam <satyamsharma2106@gmail.com>
2024-07-15 10:18:26 +0530

    add percentage sign utlity and decimel point compatibility

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const mainOperations = document.querySelectorAll('.main-operation');
 const equalsKey = document.querySelector('.equals');
 const resetButton = document.querySelector('.reset-button');
 const toggleSignButton = document.querySelector('.toggle-sign');
+const percentageButton = document.querySelector('.percentage-button');
+const decimelButton = document.querySelector('.decimel-button')
 
 numberKeys.forEach((btn) => {
     btn.addEventListener('click', () => {
@@ -22,8 +24,6 @@ numberKeys.forEach((btn) => {
             firstNumStr += numClicked;
             display.textContent = firstNumStr;
         }
-        console.log(firstNumStr);
-        console.log(secondNumStr);
     });
 });
 
@@ -33,10 +33,10 @@ mainOperations.forEach((btn) => {
         // ignore clicks if the first number has not been assigned
         if (firstNumStr) {
             operator = selectedOperator;
-            display.textContent = 0;
+            display.textContent = ''
         } 
         if (firstNumStr && secondNumStr) {
-            firstNumStr = operate(operator, parseInt(firstNumStr), parseInt(secondNumStr));
+            firstNumStr = operate(operator, parseFloat(firstNumStr), parseFloat(secondNumStr));
             secondNumStr = '';
             display.textContent = firstNumStr;
             operator = selectedOperator;
@@ -46,7 +46,8 @@ mainOperations.forEach((btn) => {
 
 equalsKey.addEventListener('click', () => {
     if (firstNumStr && secondNumStr) {
-        firstNumStr = operate(operator, parseInt(firstNumStr), parseInt(secondNumStr)).toString();
+
+        firstNumStr = operate(operator, parseFloat(firstNumStr), parseFloat(secondNumStr)).toString();
         secondNumStr = '';
         display.textContent = firstNumStr;
     }
@@ -56,23 +57,47 @@ resetButton.addEventListener('click', () => {
     firstNumStr = '';
     secondNumStr = '';
     operator = '';
-    display.textContent = 0;
+    display.textContent = ''
 });
 
 toggleSignButton.addEventListener('click', () => {
+    //if secondNumStr exists, means we are assigning value to it at the moment on the screen
     if (secondNumStr && secondNumStr.includes('-')) {
         secondNumStr = secondNumStr.replace('-', '');
     } else if (secondNumStr) {
         secondNumStr = '-' + secondNumStr;
-    } else if (firstNumStr && firstNumStr.includes('-')) {
+    }
+    // if secondNumStr doesnt exist but firstNumStr exists, means we are currently assigning to firstNumStr
+    else if (firstNumStr && firstNumStr.includes('-')) {
         firstNumStr = firstNumStr.replace('-', '');
     } else if (firstNumStr) {
         firstNumStr = '-' + firstNumStr;
     }
+    // if both don't exist, that means we haven't yet clicked a number key, so we don't want toggle key to have an effect
 
     if (secondNumStr) display.textContent = secondNumStr
     else if (firstNumStr) display.textContent = firstNumStr
 });
+
+percentageButton.addEventListener('click', () => {
+    if(secondNumStr) {
+        secondNumStr = (secondNumStr / 100).toString();
+        display.textContent = secondNumStr;
+    } else if (firstNumStr) {
+        firstNumStr = (firstNumStr / 100).toString();
+        display.textContent = firstNumStr;
+    }
+});
+
+decimelButton.addEventListener('click', () => {
+    if(secondNumStr && !(secondNumStr.includes('.'))) {
+        secondNumStr = secondNumStr + '.';
+        display.textContent = secondNumStr;
+    } else if (firstNumStr && !(secondNumStr) && !(firstNumStr.includes('.'))) {
+        firstNumStr = firstNumStr + '.';
+        display.textContent = firstNumStr;
+    }
+})
 function add(a, b) {
     return a + b;
 }
